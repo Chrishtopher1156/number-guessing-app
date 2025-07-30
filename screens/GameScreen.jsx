b9041cb7227896029ef161a674e3532d4d3a5d36
mrnia <cnia1618@gmail.com>
2025-07-30 10:31:29 +1000

    added gameScreen

---
+++ screens/GameScreen.jsx
@@ -0,0 +1,10 @@
+import { View, Text } from "react-native"
+const GameScreen = () => {
+    return (
+        <View>
+            <Text>PIGs</Text>
+        </View>
+    );
+};
+
+export default GameScreen;
